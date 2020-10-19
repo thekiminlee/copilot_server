e32993879f93595e18a5735657812d233c5d3794
Ki Min Lee <kimin@Kis-MacBook-Pro.lan>
2020-10-19 01:34:54 -0700

    add db connection

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ require('dotenv').config()
 // Dependency imports
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 // App initialization
 const app = express();
@@ -13,6 +14,16 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true}));
 
 // Route imports and config
+
+// Database connection
+mongoose.connect(process.env.DB_URI, {
+	useNewUrlParser: true,
+	useUnifiedTopology: true
+}).then(
+	() => console.log("MongoDB connected")
+).catch(
+  err => console.log("Connection unsuccesful. Error: " + err.toString())
+);
 
 // Server start
 app.listen(port, () => {
